3a4f2c1f18a98d93c925dac861865b417a32e1fd
Jack <jack20191124@proton.me>
2023-01-31 18:20:46 +0800

    Add vault install script

---
+++ scripts/install-vault.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -x
+
+# Install necessary dependencies
+sudo apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+sudo apt-get update
+
+# Setup sudo to allow no-password sudo for "hashicorp" group and adding "terraform" user
+sudo groupadd -r hashicorp
+sudo useradd -m -s /bin/bash terraform
+sudo usermod -a -G hashicorp terraform
+sudo cp /etc/sudoers /etc/sudoers.orig
+echo "terraform  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/terraform
+
+# Installing SSH key
+sudo mkdir -p /home/terraform/.ssh
+sudo chmod 700 /home/terraform/.ssh
+sudo cp /tmp/tf-packer.pub /home/terraform/.ssh/authorized_keys
+sudo chmod 600 /home/terraform/.ssh/authorized_keys
+sudo chown -R terraform /home/terraform/.ssh
+sudo usermod --shell /bin/bash terraform
+
+# Install Vault
+
+sudo -H -i -u terraform -- env bash << EOF
+sudo apt update && sudo apt install gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install vault
+EOF
